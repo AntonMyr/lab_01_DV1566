f4b87e425aa48354189cf03d7a4f219005466a78
Hugo Nolte <hugonolte@hotmail.se>
2020-11-23 13:00:25 +0100

    La till POST metod för böcker

--- api/server.py
+++ api/server.py
@@ -1,39 +1,50 @@
-import flask
+import flask, json
 from flask import request, jsonify
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
+empty_book ={
+    "id": 0,
+    "title": "null",
+    "author": "null",
+    "first_sentence": "null",
+    "year_published": 0,
+    }
 
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
 
-
 # A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+@app.route('/books/', methods=['GET', 'POST'])
 def api_all():
-    return jsonify(books)
+    if request.method == 'GET':
+        with open("./api/books.json", "r") as books:
+            novels = books.read()
+            return novels
+    else:
+        print(request.values.get('id'))
+        new_book ={
+            "id": int(request.values.get('id')),
+            "title": request.values.get('title'),
+            "author": request.values.get('author'),
+            "first_sentence": request.values.get('fs'),
+            "year_published": int(request.values.get('year')),
+        }
+        with open("./api/books.json") as json_file:
+            data = json.load(json_file)
+        
+            temp = data['books']
+
+            temp.append(new_book)
+
+        with open("./api/books.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
+
+        print(new_book)
+        return new_book
 
 app.run(host='0.0.0.0')
